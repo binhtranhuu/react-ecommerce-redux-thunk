008cbf4572221979bc1c845b8ab16999b933c77f
binhtranhuu <tranbinh241999@gmail.com>
2021-03-03 21:43:54 +0700

    Add PayPal Button

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,10 @@ app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 app.use("/api/products", productRouter);
 
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -24,7 +24,7 @@ export const orderCreateReducer = (state = {}, action) => {
 };
 
 export const orderDetailsReducer = (
-  state = { loading: true, order: {} },
+  state = { loading: true },
   action
 ) => {
   switch (action.type) {
