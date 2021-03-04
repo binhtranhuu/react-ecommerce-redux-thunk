e0412c7f61ae6cdae14f7b9118df4f6557eae41f
binhtranhuu <tranbinh241999@gmail.com>
2021-03-04 09:17:58 +0700

    List Products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,8 @@ import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import PrivateRoute from "./components/PrivateRoute";
+import AdminRoute from "./components/AdminRoute";
+import ProductListScreen from "./screens/ProductListScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -101,6 +103,10 @@ function App() {
             path="/profile"
             component={ProfileScreen}
           ></PrivateRoute>
+          <AdminRoute
+            path="/productlist"
+            component={ProductListScreen}
+          ></AdminRoute>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
