ef8274096e2d79f8d6d4a8270087948e3bd8e464
DESKTOP-M5C13OJ\tranb <tranbinh241999@gmail.com>
2021-03-07 19:26:44 +0700

    Force Order Items From One Seller

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import {
   CART_ADD_ITEM,
+  CART_ADD_ITEM_FAIL,
   CART_REMOVE_ITEM,
   CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
@@ -8,19 +9,32 @@ import {
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
   const { data } = await Axios.get(`/api/products/${productId}`);
-  dispatch({
-    type: CART_ADD_ITEM,
-    payload: {
-      name: data.name,
-      image: data.image,
-      price: data.price,
-      countInStock: data.countInStock,
-      product: data._id,
-      seller: data.seller,
-      qty,
-    },
-  });
-  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+  const {
+    cart: { cartItems },
+  } = getState();
+  if (cartItems.length > 0 && data.seller._id !== cartItems[0].seller._id) {
+    dispatch({
+      type: CART_ADD_ITEM_FAIL,
+      payload: `Can't Add To Cart. Buy only from ${cartItems[0].seller.seller.name} in this order`,
+    });
+  } else {
+    dispatch({
+      type: CART_ADD_ITEM,
+      payload: {
+        name: data.name,
+        image: data.image,
+        price: data.price,
+        countInStock: data.countInStock,
+        product: data._id,
+        seller: data.seller,
+        qty,
+      },
+    });
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(getState().cart.cartItems)
+    );
+  }
 };
 
 export const removeFromCart = (productId) => (dispatch, getState) => {
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -3,6 +3,8 @@ import {
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
   CART_SAVE_PAYMENT_METHOD,
+  CART_ADD_ITEM_FAIL,
+  CART_EMPTY,
 } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
@@ -13,16 +15,22 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
       if (existItem) {
         return {
           ...state,
+          error: "",
           cartItems: state.cartItems.map((x) =>
             x.product === existItem.product ? item : x
           ),
         };
       } else {
-        return { ...state, cartItems: [...state.cartItems, item] };
+        return { ...state, error: "", cartItems: [...state.cartItems, item] };
       }
+    case CART_ADD_ITEM_FAIL:
+      return { ...state, error: action.payload };
+    case CART_EMPTY:
+      return { ...state, error: "", cartItems: [] };
     case CART_REMOVE_ITEM:
       return {
         ...state,
+        error: "",
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
       };
     case CART_SAVE_SHIPPING_ADDRESS:
