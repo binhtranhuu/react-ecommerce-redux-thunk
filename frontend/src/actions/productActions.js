857face03e3984f8d325f75697e56bd8e8acf54f
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-03-09 16:04:22 +0700

    Complete Advanced Search

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,15 +20,21 @@ import {
   PRODUCT_UPDATE_SUCCESS,
 } from "../constants/productConstants";
 
-export const listProducts = ({ seller = "", name = "", category = "" }) => async (
-  dispatch
-) => {
+export const listProducts = ({
+  seller = "",
+  name = "",
+  category = "",
+  order = "",
+  min = 0,
+  max = 0,
+  rating = 0,
+}) => async (dispatch) => {
   dispatch({
     type: PRODUCT_LIST_REQUEST,
   });
   try {
     const { data } = await Axios.get(
-      `/api/products?seller=${seller}&name=${name}&category=${category}`
+      `/api/products?seller=${seller}&name=${name}&category=${category}&min=${min}&max=${max}&rating=${rating}&order=${order}`
     );
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,45 +1,69 @@
 import React from "react";
 
 export default function Rating(props) {
-  const { rating, numReviews } = props;
+  const { rating, numReviews, caption } = props;
   return (
     <div className="rating">
       <span>
         <i
           className={
-            rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"
+            rating >= 1
+              ? "fa fa-star"
+              : rating >= 0.5
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
       <span>
         <i
           className={
-            rating >= 2 ? "fa fa-star" : rating >= 1.5 ? "fa fa-star-half-o" : "fa fa-star-o"
+            rating >= 2
+              ? "fa fa-star"
+              : rating >= 1.5
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
       <span>
         <i
           className={
-            rating >= 3 ? "fa fa-star" : rating >= 2.5 ? "fa fa-star-half-o" : "fa fa-star-o"
+            rating >= 3
+              ? "fa fa-star"
+              : rating >= 2.5
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
       <span>
         <i
           className={
-            rating >= 4 ? "fa fa-star" : rating >= 3.5 ? "fa fa-star-half-o" : "fa fa-star-o"
+            rating >= 4
+              ? "fa fa-star"
+              : rating >= 3.5
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
       <span>
         <i
           className={
-            rating >= 5 ? "fa fa-star" : rating >= 4.5 ? "fa fa-star-half-o" : "fa fa-star-o"
+            rating >= 5
+              ? "fa fa-star"
+              : rating >= 4.5
+              ? "fa fa-star-half-o"
+              : "fa fa-star-o"
           }
         ></i>
       </span>
-      <span>{numReviews + " reviews"}</span>
+      {caption ? (
+        <span>{caption}</span>
+      ) : (
+        <span>{numReviews + " reviews"}</span>
+      )}
     </div>
   );
 }
