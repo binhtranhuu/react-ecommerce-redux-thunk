d9fb990d2e22c040871228cde4718f466775fb8c
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-07-05 22:55:17 +0700

    Email order receipt by mailgun

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -1,7 +1,13 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAdmin, isAuth, isSellerOrAdmin } from "../utils.js";
+import {
+  isAdmin,
+  isAuth,
+  isSellerOrAdmin,
+  mailgun,
+  payOrderEmailTemplate,
+} from "../utils.js";
 
 const orderRouter = express.Router();
 
@@ -73,7 +79,10 @@ orderRouter.put(
   "/:id/pay",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "email name"
+    );
     if (order) {
       order.isPaid = true;
       order.paidAt = Date.now();
@@ -86,6 +95,23 @@ orderRouter.put(
         };
       }
       const updatedOrder = await order.save();
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: "Amazona <amazona@mg.yourdomain.com>",
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html: payOrderEmailTemplate(order),
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
       res.send({ message: "Order Paid", order: updatedOrder });
     } else {
       res.status(404).send({ message: "Order Not Found" });
