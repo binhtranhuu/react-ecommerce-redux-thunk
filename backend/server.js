51238e9e2578244ebedc6f8d74ce3b97de52415b
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-07-06 18:51:39 +0700

    Connect To Socket In Server

--- backend/server.js
+++ backend/server.js
@@ -48,6 +48,54 @@ httpServer.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
 
+const users = [];
+
+io.on("connection", (socket) => {
+  console.log("connection", socket.id);
+  socket.on("disconnect", () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      console.log("Offline", user.name);
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit("updateUser", user);
+      }
+    }
+  });
+  socket.on("onLogin", (user) => {
+    const updatedUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      messages: [],
+    };
+    const existUser = users.find((x) => x._id === updatedUser._id);
+    if (existUser) {
+      existUser.socketId = socket.id;
+      existUser.online = true;
+    } else {
+      users.push(updatedUser);
+    }
+    console.log("Online", user.name);
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      io.to(admin.socketId).emit("updateUser", updatedUser);
+    }
+    if (updatedUser.isAdmin) {
+      io.to(updatedUser.socketId).emit("listUsers", users);
+    }
+  });
+
+  socket.on("onUserSelected", (user) => {
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x._id === user._id);
+      io.to(admin.socketId).emit("selectUser", existUser);
+    }
+  });
+});
+
 // app.listen(port, () => {
 //   console.log(`Serve at http://localhost:${port}`);
 // });
