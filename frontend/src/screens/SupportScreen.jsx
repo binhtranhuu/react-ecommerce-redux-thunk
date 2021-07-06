ca68ac85aa3c1f720958c22cd895aac0920da739
TRAN-HUU-BINH\tranb <tranbinh241999@gmail.com>
2021-07-07 00:04:47 +0700

    Create Admin Screen UI

---
+++ frontend/src/screens/SupportScreen.jsx
@@ -0,0 +1,89 @@
+import React, { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import MessageBox from "../components/MessageBox";
+
+let allUsers = [];
+let allMessages = [];
+let allSelectedUser = {};
+
+function SupportScreen(props) {
+  const [selectedUser, setSelectedUser] = useState({});
+  const uiMessagesRef = useRef(null);
+  const [messageBody, setMessageBody] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const selectUser = (user) => {};
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="row top full-container">
+      <div className="col-1 support-users">
+        {users.filter((x) => x._id !== userInfo._id).length === 0 && (
+          <MessageBox>No Online User Found</MessageBox>
+        )}
+        <ul>
+          {users
+            .filter((x) => x._id !== userInfo._id)
+            .map((user) => (
+              <li
+                key={user._id}
+                className={user._id === selectedUser._id ? "  selected" : "  "}
+              >
+                <button
+                  className="block"
+                  type="button"
+                  onClick={() => selectUser(user)}
+                >
+                  {user.name}
+                </button>
+                <span
+                  className={
+                    user.unread ? "unread" : user.online ? "online" : "offline"
+                  }
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div className="col-3 support-messages">
+        {!selectedUser._id ? (
+          <MessageBox>Select a user to start chat</MessageBox>
+        ) : (
+          <div>
+            <div className="row">
+              <strong>Chat with {selectedUser.name} </strong>
+            </div>
+            <ul ref={uiMessagesRef}>
+              {messages.length === 0 && <li>No message.</li>}
+              {messages.map((msg, index) => (
+                <li key={index}>
+                  <strong>{`${msg.name}: `}</strong> {msg.body}
+                </li>
+              ))}
+            </ul>
+            <div>
+              <form onSubmit={submitHandler} className="row">
+                <input
+                  value={messageBody}
+                  onChange={(e) => setMessageBody(e.target.value)}
+                  type="text"
+                  placeholder="type message"
+                />
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default SupportScreen;
